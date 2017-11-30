59d6543a30a70ef285ef74d753a676120d938fb0
Jakub Matejka <jakub@keboola.com>
2017-11-30 12:53:24 +0100

    :cop: Fix deploy to Quay

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/cli-utils quay.io/keboola/gooddata-cli-utils:$TRAVIS_TAG
-docker tag keboola/cli-utils quay.io/keboola/gooddata-cli-utils:latest
+docker tag keboola/gooddata-cli-utils quay.io/keboola/gooddata-cli-utils:$TRAVIS_TAG
+docker tag keboola/gooddata-cli-utils quay.io/keboola/gooddata-cli-utils:latest
 docker images
 docker push quay.io/keboola/gooddata-cli-utils:$TRAVIS_TAG
 docker push quay.io/keboola/gooddata-cli-utils:latest
